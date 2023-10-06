0b0d358859560ab4d679be770a070d3333068268
Mireille Mua <mireillemua1@gmail.com>
2023-10-06 11:24:50 -0400

    cart total updtaes when an item is added

--- packages/server/config/keys.js
+++ packages/server/config/keys.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   database: {
     // after the pipes will be your connection string for MongoDB Atlas which you can get from pressing connect on your collection and then clicking connect to application
-    url: process.env.MONGODB_URI || `mongodb+srv://<username>:<password>@cluster0.<cluster_key>.mongodb.net/kenziecart?retryWrites=true&w=majority`,
+    url: process.env.MONGODB_URI || `mongodb+srv://mireillemua:xPA7YyWb1LkqaLxK@cluster0.c19eumg.mongodb.net/Kenziecart?retryWrites=true&w=majority`,
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'jwt-secret',
